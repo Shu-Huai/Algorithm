e27fe232525f574f9035677c87fb1b61f21789b0
殊怀丶 <lvzhihe_123@qq.com>
2024-04-11 23:40:30 +0800

    feat: 添加循环赛日程表、线性时间选择

--- src/main/java/shuhuai/algorithm/recursive/RoundRobinSchedule.java
+++ src/main/java/shuhuai/algorithm/recursive/RoundRobinSchedule.java
@@ -29,6 +29,29 @@ public class RoundRobinSchedule {
         return table;
     }
 
+
+    public int[][] loop(int n) {
+        int[][] table = new int[n][n];
+        for (int i = 0; i < table.length; i++) {
+            table[i][0] = i + 1;
+        }
+        int r = 1;
+        int k = n;
+        while (k > 1) {
+            for (int i = 0; i < n; i = i + 2 * r) {
+                for (int s = 0; s < r; s++) {
+                    System.arraycopy(table[i + s], 0, table[i + r + s], r, r);
+                }
+                for (int s = 0; s < r; s++) {
+                    System.arraycopy(table[i + r + s], 0, table[i + s], r, r);
+                }
+                k--;
+            }
+            r *= 2;
+        }
+        return table;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
@@ -36,6 +59,13 @@ public class RoundRobinSchedule {
             int n = Integer.parseInt(s);
             RoundRobinSchedule rr = new RoundRobinSchedule();
             int[][] table = rr.recursive(n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(table[i][j] + " ");
+                }
+                System.out.println();
+            }
+            table = rr.loop(n);
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     System.out.print(table[i][j] + " ");
